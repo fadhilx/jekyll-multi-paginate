fde6659d2dcd1eff2764bc49c1b2346c10c40cb0
Fadhil Napis <fadzbobby95@gmail.com>
2017-04-27 01:15:46 +0800

    change version

--- lib/jekyll/multi/paginate/version.rb
+++ lib/jekyll/multi/paginate/version.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module Multi
     module Paginate
-      VERSION = "0.1.1"
+      VERSION = "0.1.2"
     end
   end
 end
--- test/jekyll-multi-paginate_test.rb
+++ test/jekyll-multi-paginate_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 class JekyllMultiPaginateTest < Test::Unit::TestCase
   def test_version
-  	assert_equal "0.1.1", Jekyll::Multi::Paginate::VERSION 
+  	assert_equal "0.1.2", Jekyll::Multi::Paginate::VERSION 
   end
 end
